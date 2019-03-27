c1aadfa393c84576350da63a6819b40eb033d22e
mustafaekici <wustafaekici@hotmail.com>
2019-03-27 18:58:39 +0300

    dataaccess tests

---
+++ MVPPatternDataAccess.UnitTests/DataAccessTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MVPPatternDataAccess.UnitTests
+{
+    [TestClass]
+    public class DataAccessTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception),"ConnetionString boş olamaz!")]
+        public void Ctor_ConnectionStringIsNullOrEmpty_Throws()
+        {
+            DataAccessLayer.DataAccess.MyProperty = "fakebaglanti";
+            DataAccessLayer.DataAccess data = new DataAccessLayer.DataAccess();
+
+        }
+    }
+}
